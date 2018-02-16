3ac72877296f983d4d752a7aa118e0261fc0e9a1
jtb1137 <jtb1137@gmail.com>
2018-02-16 13:08:40 -0500

    Add Users Controller and Signup Form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :programs
   root 'programs#index'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180216163956) do
+ActiveRecord::Schema.define(version: 20180216180258) do
 
   create_table "programs", force: :cascade do |t|
     t.string "name"
     t.integer "total_spots"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
