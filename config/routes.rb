212a2e675c6ce0d52f172556b1a2ffa87d4d8f5a
jtb1137 <jtb1137@gmail.com>
2018-02-16 09:43:50 -0500

    Stub out Programs Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :clients, path: 'clients'
   devise_for :trainers, path: 'trainers'
+
+  root 'programs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # Define root route
 end
